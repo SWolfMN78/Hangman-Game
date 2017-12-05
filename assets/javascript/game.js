3ee9e88a04a95c00a112e95b49d269739b95c29d
Wolfknightx <Wolfknightx@Gmail.com>
2017-12-05 15:58:47 -0600

    new update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,31 @@ var wordList1 = ["dog", "wolf", "demon", "dragon", "demagorgan"];
 var wordList2 = ["mage", "thief", "knight", "paladin", "sorcerer"];
 var wordList3 = ["bat", "mace", "sword", "dagger", "halbard"];
 
-function randomreader(arr){
+//practice function code for looping through an array.
+function listReader(arr){
 	for (var i = 0; i < arr.length; i++){
 		console.log(arr[i]);
 	}
 	console.log("--------")
 }
+listReader(wordList1);
+listReader(wordList2);
+listReader(wordList3);
 
-randomreader(wordList1);
-randomreader(wordList2);
-randomreader(wordList3);
+/*Index information - user able to press enter to start the game into the next
+screen as an alternitive to pressing the buttons below.*/
+
+
+/*gameDisplay info - meat of programming will go here.
+things to be added: -User will be given a word from the above arraylist to select from.
+					-Key up the buttons to help with navigation.
+					-handle adding an element to the screen for each right 
+					or wrong answer.
+					-All of the rules for the game will be stored here
+					-When the user gets a word correct give them a point for a won game.
+					-When a user gets hung give them a point for a lost game.
+					-If a game is lost then ask if they wish to continue.
+*/
+
+/*gameOver display info - final screen data will go here.
+*/
